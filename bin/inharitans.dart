01aed7332bc7ee3b7ca073d5e866e12570372de8
hanihassan01 <hani77327@gmail.com>
2023-08-21 12:21:36 +0530

    tgb

---
+++ bin/inharitans.dart
@@ -0,0 +1,22 @@
+
+
+
+void main(){
+  As T=As();
+  T.asd();
+}
+
+class As extends B{
+  void asd(){
+    int a=20;
+  print(super.a);
+  
+  }
+}
+class B{
+
+
+int a=30;
+
+ 
+}
---
+++ test/inharitans_test.dart
@@ -0,0 +1,8 @@
+import 'package:inharitans/inharitans.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
